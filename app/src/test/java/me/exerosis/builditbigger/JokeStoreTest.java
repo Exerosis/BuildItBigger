97491e5ab98bb12c964c13d514f8fda2da4d0a69
TheExerosisGaming <theexerosisgaming@gmail.com>
2017-05-04 18:29:30 -0400

    ~Test

--- app/src/test/java/me/exerosis/builditbigger/JokeStoreTest.java
+++ app/src/test/java/me/exerosis/builditbigger/JokeStoreTest.java
@@ -28,7 +28,7 @@ public class JokeStoreTest {
         JokeStore jokeStore = new Retrofit.Builder().
                 addConverterFactory(GsonConverterFactory.create()).
                 addCallAdapterFactory(RxJavaCallAdapterFactory.createAsync()).
-                baseUrl("http://10.60.12.125:8080/").build().create(JokeStore.class);
+                baseUrl("http://localhost:8080/").build().create(JokeStore.class);
 
         Joke joke = jokeStore.getJoke().toBlocking().first();
         assertNotNull(joke);
